7dea56620b242f6cd89b85079e1265db46de33ae
Raja0514 <rajamr1987@gmail.com>
2022-08-06 12:13:05 +0400

    last

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express=require('express')
 const app=express();
-app.get('/',function(res,req){
-
-    res.send("hello world")
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+app.listen(process.env.PORT||3000,()=>{
+    console.log('server running')
 })
-app.listen(process.env.PORT||3000)
